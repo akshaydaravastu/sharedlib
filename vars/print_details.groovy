c38efa25b8a3f8b6aa91255a0deb9225d4f8bf49
akshaydaravastu <64062260+akshaydaravastu@users.noreply.github.com>
2021-12-31 17:05:51 +0530

    Update print_details.groovy

--- vars/print_details.groovy
+++ vars/print_details.groovy
@@ -1,4 +1,12 @@
 def call(group_name,version,filetype){
   echo "group is ${group_name} \n versioning is ${version} \n type is ${filetype}"
+  script{
+                    def filepath='example/demo/java/spring.boot/1.2.0-snapshot'
+                    def version_filetype=filepath.split("/")[-1]
+                    def version=version_filetype.split("-")[0]
+                    def filetype=version_filetype.split("-")[1]
+                    def group_name=filepath.split("/")[-2]
+                 
+                }
 }
 
